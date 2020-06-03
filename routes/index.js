3ca6b02d3a3efa01b7608daa50461b6b1dcb8718
acostade <acostade@gmail.com>
2020-06-03 15:59:35 -0500

    done

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@ const todosCtrl = require('../controllers/todos');
 
 router.get('/', todosCtrl.index);
 router.post('/todos', todosCtrl.create);
-router.delete('/todos/idx', todosCtrl.delete);
+router.delete('/todos/:idx', todosCtrl.delete);
 
 module.exports = router;
